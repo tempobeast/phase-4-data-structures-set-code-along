cd04364644c6f4fd1564d303d88b54c7d6b81526
Zach <shaw.zacht@gmail.com>
2022-08-04 12:41:32 -0700

    done

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,31 @@
-# your code here
+class MySet
+
+    attr_accessor: :hash
+
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+
+end
